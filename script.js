c63b1ee3b51948bbf1edb7bf980b3bcde83171bb
Beatrice <brkovalik@gmail.com>
2026-04-27 14:07:18 -0400

    im so done already

---
+++ script.js
@@ -0,0 +1,15 @@
+const cookie = document.getElementById("cookie")
+const inventory = document.getElementById("inventory")
+
+cookie.addEventListener("click", function() {
+    console.log("aioufai")
+})
+
+class inventory_items {
+    constructor() {
+        //image
+        //benifit
+        //interval
+    }
+    //add to inventory
+}
