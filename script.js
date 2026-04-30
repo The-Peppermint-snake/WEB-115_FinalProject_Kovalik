c2086d24a5c9ec402bf483e3a55512425db80d0b
Beatrice <brkovalik@gmail.com>
2026-04-30 14:06:22 -0400

    im am so confused

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 //     -add cursors to the store    
 //          -append as children of the shop div
 //          -add store css class when doing it
-//     -figure out how to import shopitems
-//         -where would I import to?
 //     -hover event shows the discription text for each item (tool tip?)
 //     -interval functions for the cursors that update total cookies
 //     -fancy css for the cursors 
 //         -want them to surrond the cookie Picture 
-//             -static at first maybe  try to make them move
+//             -static at first maybe try to make them move
 //      -update reset button to include inventory
 //      -for loop after page refresh to add inventory items back
 
@@ -18,11 +16,27 @@ const cookie = document.getElementById("cookie");
 const inventory = document.getElementById("inventory");
 const cookieCounter = document.getElementById("cookieCounter");
 const resetButton = document.getElementById("reset");
-// const chocChip = document.getElementById("chip");
-// const doubleChoc = document.getElementById("double");
-// const sugar = document.getElementById("sugar")
+const shopDiv = document.getElementById("store")
 
-//import {chocChipCursor,doubleChocCursor,sugarCursor} from "shopItems.js";
+import {shopArray} from "./shopItems.js";
+
+for (let i = 0; i < shopArray.length; i++) {
+    let imgDiv = document.createElement('div');
+    let newItem = document.createElement("img")
+    let newText = document.createElement("span")
+    newItem.src = shopArray[i].img
+    newItem.classList.add("shopImage")
+    imgDiv.classList.add("tooltip")
+    // imgDiv.textContent = "hover"
+    newItem.addEventListener("click", function() {
+        //let cost = shopArray[i].
+    })
+    newText.classList.add("tooltiptext")
+    newText.innerHTML = shopArray[i].discription
+    imgDiv.appendChild(newItem)
+    imgDiv.appendChild(newText)
+    shopDiv.appendChild(imgDiv)
+}
 
 cookieCounter.innerHTML = "Your total cookies are: " + Number(localStorage.getItem(cookieStore))
 
@@ -34,13 +48,9 @@ resetButton.addEventListener("click", function() {
 cookie.addEventListener('click', function() {
     cookie.classList.add('spin');
     let newNum = Number(localStorage.getItem(cookieStore)) + 1
-    totalCookie = localStorage.setItem(cookieStore, newNum)
+    let totalCookie = localStorage.setItem(cookieStore, newNum)
     cookieCounter.innerHTML = "Your total cookies are: " + newNum;
 });
 cookie.addEventListener('animationend', function() {
 	cookie.classList.remove('spin');
 });
-
-
-
-
--- shopItems.js
+++ shopItems.js
@@ -4,21 +4,22 @@ class inventory_items {
         this.interval = interval;
         this.cost = cost;
         this.discription = discription;
+        this.buy = function() {
+            console.log(discription)
+        };
         //event for adding inventory
         //hover event shows the discription 
     }
     //add/display to inventory
 }
 
-let chocChipCursor = new inventory_items("images/cookieCursor1.png",10,20,"This basic chocolate cookie will click every 10 seconds")
-let doubleChocCursor = new inventory_items("images/cookieCursor2.png", 8, 40, "This double chocolate chip cursor will click every 8 seconds")
-let sugarCursor = new inventory_items("images/cookieCursor3.png", 6, 60, "This fresh sugar cookie will click every 6 seconds")
-const store = document.getElementById("store")
+let chocChipCursor = new inventory_items("images/cookieCursor1.png",10,20,"This basic chocolate cookie will click every 10 seconds");
+let doubleChocCursor = new inventory_items("images/cookieCursor2.png", 8, 40, "This double chocolate chip cursor will click every 8 seconds");
+let sugarCursor = new inventory_items("images/cookieCursor3.png", 6, 60, "This fresh sugar cookie will click every 6 seconds");
 
+let shopArray = [chocChipCursor,doubleChocCursor,sugarCursor];
 
-
-
-export {chocChipCursor,doubleChocCursor,sugarCursor};
+export {shopArray};
 
 // sugar.addEventListener("click", function() {
 //     totalCookie = localStorage.getItem(cookieStore);
