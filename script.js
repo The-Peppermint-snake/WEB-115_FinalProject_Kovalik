eac133a1e819f276a2ae6db30310d342a279f383
Beatrice <brkovalik@gmail.com>
2026-05-05 14:10:16 -0400

    this is why comp sci is not for me :)))))

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
 //  to-do
-//     -add cursors to the store    
-//          -append as children of the shop div
-//          -add store css class when doing it
-//     -hover event shows the discription text for each item (tool tip?)
 //     -interval functions for the cursors that update total cookies
 //     -fancy css for the cursors 
 //         -want them to surrond the cookie Picture 
 //             -static at first maybe try to make them move
 //      -update reset button to include inventory
 //      -for loop after page refresh to add inventory items back
-
+//      -add inventory to local storage
 
 
 const cookie = document.getElementById("cookie");
-const inventory = document.getElementById("inventory");
+const inventoryDiv = document.getElementById("inventoryDiv");
 const cookieCounter = document.getElementById("cookieCounter");
 const resetButton = document.getElementById("reset");
 const shopDiv = document.getElementById("store")
+let inventory = []
 
 import {shopArray} from "./shopItems.js";
-
 for (let i = 0; i < shopArray.length; i++) {
-    let imgDiv = document.createElement('div');
-    let newItem = document.createElement("img")
+    let newDiv = document.createElement('div');
+    let newImg = document.createElement("img")
     let newText = document.createElement("span")
-    newItem.src = shopArray[i].img
-    newItem.classList.add("shopImage")
-    imgDiv.classList.add("tooltip")
-    // imgDiv.textContent = "hover"
-    newItem.addEventListener("click", function() {
-        //let cost = shopArray[i].
+    newImg.src = shopArray[i].img
+    newImg.classList.add("shopImage")
+    newDiv.classList.add("tooltip")
+    newImg.addEventListener("click", function() {
+        let currentCookie = Number(localStorage.getItem("cookies"))
+        let newCookie = currentCookie - shopArray[i].cost
+        if (newCookie < 0) {
+            window.alert("You need more cookies!")
+        } else {
+            inventory.push(shopArray[i])
+            let inventoryStore;
+            inventoryStore = localStorage.setItem("inventory", JSON.stringify(inventory));
+            let totalCookie = localStorage.setItem("cookies", newCookie)
+            cookieCounter.innerHTML = "Your total cookies are: " + newCookie;
+        }
     })
     newText.classList.add("tooltiptext")
-    newText.innerHTML = shopArray[i].discription
-    imgDiv.appendChild(newItem)
-    imgDiv.appendChild(newText)
-    shopDiv.appendChild(imgDiv)
+    newText.innerHTML = shopArray[i].description
+    newDiv.appendChild(newImg)
+    newDiv.appendChild(newText)
+    shopDiv.appendChild(newDiv)
 }
 
-cookieCounter.innerHTML = "Your total cookies are: " + Number(localStorage.getItem(cookieStore))
+cookieCounter.innerHTML = "Your total cookies are: " + Number(localStorage.getItem("cookies"))
 
 resetButton.addEventListener("click", function() {
-    localStorage.setItem(cookieStore, 0)
-    cookieCounter.innerHTML = "Your total cookies are: " + Number(localStorage.getItem(cookieStore))
+    localStorage.setItem("cookies", 0)
+    localStorage.setItem("inventory", [])
+    cookieCounter.innerHTML = "Your total cookies are: " + Number(localStorage.getItem("cookies"))
 })
 
 cookie.addEventListener('click', function() {
     cookie.classList.add('spin');
-    let newNum = Number(localStorage.getItem(cookieStore)) + 1
-    let totalCookie = localStorage.setItem(cookieStore, newNum)
+    let newNum = Number(localStorage.getItem("cookies")) + 1
+    let totalCookie = localStorage.setItem("cookies", newNum)
     cookieCounter.innerHTML = "Your total cookies are: " + newNum;
 });
 cookie.addEventListener('animationend', function() {
--- shopItems.js
+++ shopItems.js
@@ -1,28 +1,16 @@
-class inventory_items {
-    constructor(img,interval,cost,discription) {
+class shopItem {
+    constructor(img,interval,cost,description) {
         this.img = img;
         this.interval = interval;
         this.cost = cost;
-        this.discription = discription;
-        this.buy = function() {
-            console.log(discription)
-        };
-        //event for adding inventory
-        //hover event shows the discription 
+        this.description = description;
     }
-    //add/display to inventory
 }
 
-let chocChipCursor = new inventory_items("images/cookieCursor1.png",10,20,"This basic chocolate cookie will click every 10 seconds");
-let doubleChocCursor = new inventory_items("images/cookieCursor2.png", 8, 40, "This double chocolate chip cursor will click every 8 seconds");
-let sugarCursor = new inventory_items("images/cookieCursor3.png", 6, 60, "This fresh sugar cookie will click every 6 seconds");
+let chocChipCursor = new shopItem("images/cookieCursor1.png",10,20,"This super duper basic chocolate cookie will click every 10 seconds. Costs: 20");
+let doubleChocCursor = new shopItem("images/cookieCursor2.png", 8, 40, "This double chocolate chip cursor will click every 8 seconds. Costs: 40");
+let sugarCursor = new shopItem("images/cookieCursor3.png", 6, 60, "This fresh sugar cookie will click every 6 seconds. Costs: 60");
 
 let shopArray = [chocChipCursor,doubleChocCursor,sugarCursor];
 
 export {shopArray};
-
-// sugar.addEventListener("click", function() {
-//     totalCookie = localStorage.getItem(cookieStore);
-//     totalCookie -= 10
-//     console.log(totalCookie)
-// })
